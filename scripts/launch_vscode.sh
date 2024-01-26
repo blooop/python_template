d3a276b381d1a3ecb19c17de3c6dd23ba3065242
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2024-01-26 09:54:55 +0000

    not working fully

--- scripts/launch_vscode.sh
+++ scripts/launch_vscode.sh
@@ -1,3 +1,20 @@
 #! /bin/bash
+set -e
 
-rocker --x11 --user --home --pull --name mycont ubuntu:22.04 & ./code-remove-container .
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #gets the current directory
+cd $SCRIPT_DIR/..
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #gets the current directory
+
+CONTAINER_NAME=${PWD##*/}  
+
+echo "stopping existing container" $CONTAINER_NAME 
+docker stop $CONTAINER_NAME || true
+
+container_hex_id=$(printf $CONTAINER_NAME | xxd -p | tr '\n' ' ' | sed 's/\\s//g' | tr -d ' ');
+
+# rocker --x11 --user --home --pull --name $CONTAINER_NAME --mode non-interactive ubuntu:22.04 
+# rocker --x11 --user --home --pull --name $CONTAINER_NAME ubuntu:22.04 
+rocker --nvidia --x11 --user --home --pull --git --name $CONTAINER_NAME --oyr-run-arg " --detach" ubuntu:22.04 
+
+
+code --folder-uri vscode-remote://attached-container+$container_hex_id/$PROJECT_DIR
